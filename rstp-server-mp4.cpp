b3713b125b4e219f30047a3fb7a85cf6641faa8f
shafeervc <43771757+shafeervc@users.noreply.github.com>
2018-10-02 14:22:16 +0900

    Update rstp-server-mp4.cpp

--- rstp-server-mp4.cpp
+++ rstp-server-mp4.cpp
@@ -1,22 +1,3 @@
-/* GStreamer
-* Copyright (C) 2008 Wim Taymans <wim.taymans at gmail.com>
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Library General Public
-* License as published by the Free Software Foundation; either
-* version 2 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Library General Public License for more details.
-*
-* You should have received a copy of the GNU Library General Public
-* License along with this library; if not, write to the
-* Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-* Boston, MA 02110-1301, USA.
-*/
-
 #include "stdafx.h"
 #include <gst/gst.h>
 #include <gst/rtsp-server/rtsp-server.h>
@@ -168,7 +149,7 @@ main(int argc, char *argv[]) {
 	* element with pay%d names will be a stream */
 	factory = gst_rtsp_media_factory_new();
 	
-	/* *************************** To Remove ***************************************************************** */
+	/* *************************** To Remove *************this code block is to analyze manual pipeline creation ** Not functional as of now**************************************************** */
 		GstElement *pipeline;
 		GstElement *videosrc, *demuxer, *video_encode, *video_convert, *rtppay, *sink;
 		GstBus *bus;
